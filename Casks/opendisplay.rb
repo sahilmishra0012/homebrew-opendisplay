0ada20199637df9cfabdff42fd434d2e0c2b938a
Sahil Mishra <sahilmsr@amazon.com>
2026-04-03 13:51:02 +0530

    Update to v1.2.0

--- Casks/opendisplay.rb
+++ Casks/opendisplay.rb
@@ -1,6 +1,6 @@
 cask "opendisplay" do
   version "1.2.0"
-  sha256 "2a727851ad004f159a039aeadeee2a4680c4e5aeca7315ecd0c42be0a26629af"
+  sha256 "837b857fe6255166ac3e782daaa31c8f8ba5847d355ee15eafec53601fe0ab14"
 
   url "https://github.com/sahilmishra0012/OpenDisplay/releases/download/v#{version}/OpenDisplay-#{version}.zip"
   name "OpenDisplay"
